925e717074dd0cf733239a3afb95622c2117df1b
Afzal Zubair <afzal.zubair@khi.iba.edu.pk>
2024-05-30 23:03:37 +0100

    Create Test.jsx

---
+++ src/icons/Test.jsx
@@ -0,0 +1,18 @@
+const TestIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M3 3h18v18H3z" />
+    <path d="M9 9h6v6H9z" />
+  </svg>
+);
+
+export default TestIcon;
