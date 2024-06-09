10deef239104001605ce657f6446d10892b07b6a
Afzal Zubair <49808043+afzalzbr@users.noreply.github.com>
2024-06-09 11:04:12 +0100

    Upload Summary

--- src/icons/WeightDynamic.jsx
+++ src/icons/WeightDynamic.jsx
@@ -11,19 +11,28 @@ const WeightDynamic = (props) => (
     {...props}
   >
     <path
-      d="M3 20.4V3.6C3 3.26863 3.26863 3 3.6 3H20.4C20.7314 3 21 3.26863 21 3.6V20.4C21 20.7314 20.7314 21 20.4 21H3.6C3.26863 21 3 20.7314 3 20.4Z"
-      stroke="black"
-      strokeWidth={1.5}
-    />
-    <path
-      d="M6 18L18 18"
+      d="M19 3L5 3C3.89543 3 3 3.89543 3 5L3 19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z"
       stroke="black"
       strokeWidth={1.5}
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      d="M12 14V6M12 6L15.5 9.5M12 6L8.5 9.5"
+      d="M7 7L17 7"
+      stroke="black"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M7 12L17 12"
+      stroke="black"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M7 17L13 17"
       stroke="black"
       strokeWidth={1.5}
       strokeLinecap="round"
