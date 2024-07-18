cd366cecd51c755ee410533d1323dd3f348971a1
Afzal Zubair <49808043+afzalzbr@users.noreply.github.com>
2024-07-18 16:58:24 +0100

    Message Text Icon

--- src/icons/WeightDynamic.jsx
+++ src/icons/WeightDynamic.jsx
@@ -11,35 +11,23 @@ const WeightDynamic = (props) => (
     {...props}
   >
     <path
-      d="M4 19V5C4 3.89543 4.89543 3 6 3H19.4C19.7314 3 20 3.26863 20 3.6V16.7143"
-      stroke="black"
-      strokeWidth={1.5}
-      strokeLinecap="round"
-    />
-    <path
-      d="M6 17L20 17"
-      stroke="black"
-      strokeWidth={1.5}
-      strokeLinecap="round"
-    />
-    <path
-      d="M6 21L20 21"
-      stroke="black"
-      strokeWidth={1.5}
-      strokeLinecap="round"
-    />
-    <path
-      d="M6 21C4.89543 21 4 20.1046 4 19C4 17.8954 4.89543 17 6 17"
+      d="M7 12L17 12"
       stroke="black"
       strokeWidth={1.5}
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      d="M9 7L15 7"
+      d="M7 8L13 8"
       stroke="black"
       strokeWidth={1.5}
       strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M3 20.2895V5C3 3.89543 3.89543 3 5 3H19C20.1046 3 21 3.89543 21 5V15C21 16.1046 20.1046 17 19 17H7.96125C7.35368 17 6.77906 17.2762 6.39951 17.7506L4.06852 20.6643C3.71421 21.1072 3 20.8567 3 20.2895Z"
+      stroke="black"
+      strokeWidth={1.5}
     />
   </svg>
 );
