43b112bb4ad30bbcd1b7f40518b1b94b817d8a04
Afzal Zubair <49808043+afzalzbr@users.noreply.github.com>
2024-07-18 16:57:19 +0100

    Book Icon

--- src/icons/WeightDynamic.jsx
+++ src/icons/WeightDynamic.jsx
@@ -11,18 +11,35 @@ const WeightDynamic = (props) => (
     {...props}
   >
     <path
-      d="M2 8L11.7317 3.13416C11.9006 3.04971 12.0994 3.0497 12.2683 3.13416L22 8"
+      d="M4 19V5C4 3.89543 4.89543 3 6 3H19.4C19.7314 3 20 3.26863 20 3.6V16.7143"
+      stroke="black"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+    />
+    <path
+      d="M6 17L20 17"
+      stroke="black"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+    />
+    <path
+      d="M6 21L20 21"
+      stroke="black"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+    />
+    <path
+      d="M6 21C4.89543 21 4 20.1046 4 19C4 17.8954 4.89543 17 6 17"
       stroke="black"
       strokeWidth={1.5}
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      d="M20 11V19C20 20.1046 19.1046 21 18 21H6C4.89543 21 4 20.1046 4 19V11"
+      d="M9 7L15 7"
       stroke="black"
       strokeWidth={1.5}
       strokeLinecap="round"
-      strokeLinejoin="round"
     />
   </svg>
 );
